51db34adaf08b57b831b823619ca802cb4edf95c
Eduardo <eddev13@outlook.com>
2025-07-01 07:52:31 -0300

    feat: Adiciona suporte a CORS na API

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,11 +3,12 @@ import { Request, Response } from "express";
 import linkRoutes from "./routes/link.routes";
 import requestLogger from "./middlewares/requestLogger";
 import errorHandler from "./middlewares/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(requestLogger);
 
 app.use("/api", linkRoutes);
